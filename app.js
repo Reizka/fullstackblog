1294be8dd731c8d28249d89534a63a7f2ce2951c
Renny Lindberg <reizka.deepone@gmail.com>
2019-10-28 19:59:29 +0100

    start

---
+++ app.js
@@ -0,0 +1,45 @@
+
+
+const http = require('http')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config');
+const middleware = require('./utils/middleware');
+//const personsRouter = require('./controllers/persons');
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+
+
+console.log('connecting to', config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI, { useNewUrlParser: true })
+  .then(result => {
+    console.log('connected to MongoDB');
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message);
+  });
+
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static('build'));
+
+app.use(middleware.requestLogger);
+
+
+//app.use('/api/persons',personsRouter);
+
+personsRouter.use(middleware.unknownEndpoint);
+personsRouter.use(middleware.errorHandler);
+
+module.exports = app;
